b47febeb85f87831afce5b6c6789b07676169771
Saul Alfaro <saul.alfaro.223@my.csun.edu>
2025-04-19 22:42:24 -0700

    Password check added, html page changes

--- demo/demo/src/main/java/com/example/demo/PageController.java
+++ demo/demo/src/main/java/com/example/demo/PageController.java
@@ -1,7 +1,12 @@
-@Controller 
-  public class PageController {
-    @GetMapping("/index")
-    public String indexPage(){
-      return "index";
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+    @GetMapping("/")
+    public String redirectToHome() {
+        return "redirect:/index.html";
     }
-  }
+}
